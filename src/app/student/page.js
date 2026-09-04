1a488ee18fc40379961bee4357ef1acc86ec9b19
Nimeshi Disanayaka <nishinimeshi96@gmail.com>
2026-09-04 12:24:07 +0530

    done

--- src/app/student/page.js
+++ src/app/student/page.js
@@ -1,7 +1,7 @@
 export default function Student() {
   return (
     <div style={{ padding: '50px', fontSize: '24px' }}>
-      <h1>Hello Student</h1>
+      <h1>Student Page</h1>
     </div>
   );
 }
